91f8bab5dfc60d91f7c1cdec98fe401308772ae9
SproutX-Scripter <aroaenix@gmail.com>
2025-06-11 22:28:58 +0800

    Create GrowAGardenMain.lua

---
+++ GrowAGardenMain.lua
@@ -0,0 +1,5 @@
+-- SproutX GrowHub - Grow A Garden Script
+print("🌿 Welcome to SproutX - Grow A Garden Script loaded!")
+
+-- Place your GrowAGarden code here:
+loadstring(game:HttpGet("https://paste.ee/r/SqMPhAE9"))()
